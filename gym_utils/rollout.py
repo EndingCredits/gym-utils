0dde6a1d2429bf47d4dc7018f0745c92bf653a29
Ending_Credits <awwoof@hotmail.com>
2018-06-26 16:04:04 +0100

    Added n-step return to rollout

--- gym_utils/rollout.py
+++ gym_utils/rollout.py
@@ -84,8 +84,39 @@ class Rollout():
                      + [ self._curr_sart[STATE] ]
           return states
       
-  def n_step_return(self):
-    pass #Not implemented yet
+      
+  def n_step_return(self, n_steps, discount=1, value_index=None):
+    '''
+    Calculates the n-step return based on value predictions saved in self.others
+    
+    n_steps is the number of steps before truncation
+    discount is the discount factor
+    value_index is the index in the list of others where the values are stored,
+    leave none is each others is a raw float value corresponding to the value
+    '''
+    
+    if value_index is None:
+        values = self.others
+    else:
+        values = [ o[value_index] for o in self.others ]
+
+    returns = []
+    
+    for t in range(len(self)):
+      if self.current_step - t > n_steps:
+        #Get truncated return
+        start_t = t + n_steps
+        R_t = values[start_t]
+      else:
+        start_t = self.current_step
+        R_t = 0
+          
+      for i in range(start_t-1, t, -1):
+        R_t = R_t * discount + self.rewards[i]
+      returns.append(R_t)
+    return returns
+
+
   
       
       
